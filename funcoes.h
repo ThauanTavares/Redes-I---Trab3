11de9a0dd82d36f3e5510b8acf7c8c4afcb11618
Thauan <60517334+ThauanTavares@users.noreply.github.com>
2023-01-31 18:39:40 -0300

    Add files via upload

---
+++ funcoes.h
@@ -0,0 +1,9 @@
+#ifndef __FUNCOES_H__
+#define __FUNCOES_H__
+
+void gerandoInterferencia(char **mensagem,unsigned int tamC, int tamI);
+int paridadeLinha(char** mensagem,int linha ,unsigned int tam);
+int paridadeColuna(char** mensagem,int coluna ,unsigned int tam);
+
+int ehPar(int aux);
+#endif
