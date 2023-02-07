8fd21828ea290f846139854f0bcf0f7081c1273f
Thauan <60517334+ThauanTavares@users.noreply.github.com>
2023-02-07 14:04:29 -0300

    Delete funcoes.h

--- funcoes.h
+++
@@ -1,9 +0,0 @@
-#ifndef __FUNCOES_H__
-#define __FUNCOES_H__
-
-void gerandoInterferencia(char **mensagem,unsigned int tamC, int tamI);
-int paridadeLinha(char** mensagem,int linha ,unsigned int tam);
-int paridadeColuna(char** mensagem,int coluna ,unsigned int tam);
-
-int ehPar(int aux);
-#endif
